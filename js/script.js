fc6937676555c6bf4a793d0cbaecd54383f8b374
analima0702 <ana.9563765@aluno.mg.gov.br>
2026-03-19 14:19:26 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 let likeCount = 0;
+let curtido = false; //flag booleana
+
 
 function curtir() {
+ 
+if(curtido == false){
   likeCount++;
-  document.getElementById("likeCount").innerText = likeCount;
+  curtido = true;
+  DocumentType.getElementById("LikeCount").innerText = likeCount;
+} else{
+  likeCount--;
+  curtido = false;
+  DocumentType.getElementById("LikeCount").innerText = likeCount;
+}
+
+
 }
 
 document.getElementById("likeBtn").addEventListener("click", curtir);
